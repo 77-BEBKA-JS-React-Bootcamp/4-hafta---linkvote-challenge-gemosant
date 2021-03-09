589e1062a21eebe5de0480303cc69935baafea7d
gemosant <rasim.mutlu@outlook.com>
2021-03-10 01:48:03 +0300

    created basic struct

---
+++ link-vote-challenge/src/Components/List/List.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './List.css'
+
+const List = () => {
+            return(
+        <div className='list'>
+            <ul className='list-items'>
+                <li className='item'>item-1</li>
+                <li className='item'>item-3</li>
+                <li className='item'>item-2</li>
+            </ul>
+        </div>
+    )
+}
+
+export default List
